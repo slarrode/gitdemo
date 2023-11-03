2e90a0b5a87de218ac6c7191b28003ef1b5b1d63
slarrode <slarrode@gmail.com>
2023-11-03 12:19:00 +0100

    .

---
+++ algo..py
@@ -0,0 +1,9 @@
+
+def megusta(algo):
+    print("algo haremos... y {algo} tambien")
+
+if __name__ == __main__:
+
+    megusta("otra cosa")
+
+
---
+++ makefile
@@ -0,0 +1,5 @@
+all
+	
+
+clean:
+	rm *.exe *.o a.out
